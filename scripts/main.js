51cba43385eeba62330694bd131cb5804ead2506
Tyler Liu <tyler@nanoleaf.me>
2016-12-20 20:07:22 -0500

    added add function

--- scripts/main.js
+++ scripts/main.js
@@ -21,14 +21,30 @@ var App = React.createClass({
     this.state.fishes[`fish-${timeStamp}`] =fish
     this.setState({fishes: this.state.fishes})
   },
+  loadSamples:function () {
+    this.setState({
+      fishes:require('./sample-fishes')
+    })
+  },
+  RenderFish:function (key) {
+    return <Fish key={key} index={key} details ={this.state.fishes[key]} addToOrder={this.addToOrder} />
+  },
+  addToOrder: function (key) {
+
+    this.state.order[key] = this.state.order[key] + 1 || 1
+    this.setState({order:this.state.order })
+  },
   render: function() {
     return (
       <div className="catch-of-the-day">
         <div>
           <Header tagline="Fresh Seafood Market"/>
+          <ul className="list-of-fishes">
+            {Object.keys(this.state.fishes).map(this.RenderFish)}
+          </ul>
         </div>
         <Order/>
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
@@ -76,6 +92,26 @@ var Header = React.createClass({
   }
 })
 
+var Fish = React.createClass({
+  onButtonClick: function () {
+    this.props.addToOrder(this.props.index)
+  },
+  render:function () {
+    var details = this.props.details
+    var isAvailable =(details.status === "available" ? true : false)
+    var buttonText = isAvailable ? 'Add To Order' : 'Sold Out'
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name}/>
+        <h3 className="fish-name">{details.name}
+        <span className="price">{helpers.formatPrice(details.price)}</span></h3>
+        <p>{details.desc}</p>
+        <button onClick={this.onButtonClick}>{buttonText}</button>
+      </li>
+    )
+  }
+})
+
 
 var Order = React.createClass({
   render: function() {
@@ -92,6 +128,7 @@ var Inventory = React.createClass({
       <div>
         <h2>Inventory</h2>
         <AddFishForm {...this.props}/>
+        <button onClick={this.props.loadSamples}>Load Sample Fishe</button>
       </div>
 
     )
